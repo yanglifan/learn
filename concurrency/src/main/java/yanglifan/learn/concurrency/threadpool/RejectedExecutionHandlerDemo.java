4a9c63befc741d424770db267814a1c9679c1837
aliyang <yanglifan@gmail.com>
2015-02-21 18:26:15 +0800

    test_caller_runs_policy

--- concurrency/src/main/java/yanglifan/learn/concurrency/threadpool/RejectedExecutionHandlerDemo.java
+++ concurrency/src/main/java/yanglifan/learn/concurrency/threadpool/RejectedExecutionHandlerDemo.java
@@ -1,27 +1,59 @@
 package yanglifan.learn.concurrency.threadpool;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RejectedExecutionHandlerDemo {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RejectedExecutionHandlerDemo.class);
+
+    private ThreadPoolExecutor executor;
+
+    @Before
+    public void setUp() throws Exception {
+        executor = (ThreadPoolExecutor) ExecutorBuilder.newBuilder().corePoolSize(1)
+                .workQueue(new ArrayBlockingQueue<>(1)).build();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+    }
 
     @Test(expected = RejectedExecutionException.class)
-    public void test_abort_policy() {
-        ExecutorService executor = ExecutorBuilder.newBuilder().corePoolSize(1)
-                .workQueue(new ArrayBlockingQueue<>(1)).build();
+    public void test_abort_policy() throws Exception {
+        invokeRejectHandler();
+        System.out.println("Main thread exec");
+    }
+
+    @Test
+    public void test_caller_runs_policy() throws Exception {
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        invokeRejectHandler();
+    }
+
+    private void invokeRejectHandler() throws Exception {
         executor.execute(() -> {
             try {
-                Thread.sleep(10000);
+                LOGGER.info("Sleep 3s");
+                Thread.sleep(3000);
+                LOGGER.info("Sleep task");
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
 
-        executor.execute(() -> System.out.println("Empty task"));
-        executor.execute(() -> System.out.println("Empty task"));
-        System.out.println("Main thread exec");
+        executor.execute(() -> LOGGER.info("Task 1"));
+        LOGGER.info("Before Task 2");
+        executor.execute(() -> LOGGER.info("Task 2"));
     }
+
 }
