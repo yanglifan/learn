17d65be451a8d2e8b05f8ea32afba87cc72f6b8c
Lifan Yang <yanglifan@gmail.com>
2015-03-16 14:29:42 +0800

    Add string to inputstream example

--- core/src/main/java/yanglifan/learn/core/io/String2InputStream.java
+++ core/src/main/java/yanglifan/learn/core/io/String2InputStream.java
@@ -1,7 +1,22 @@
 package yanglifan.learn.core.io;
 
-/**
- * Created by plcm on 3/16/15.
- */
+import java.io.*;
+
 public class String2InputStream {
+    public static void main(String[] args) throws IOException {
+        String str = "This is a String ~ GoGoGo";
+
+        // convert String into InputStream
+        InputStream is = new ByteArrayInputStream(str.getBytes());
+
+        // read it with BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        br.close();
+    }
 }
