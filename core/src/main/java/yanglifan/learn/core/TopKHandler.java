78a8501e0b234dd10fda8a90b54d954e993eea80
Lifan Yang <yanglifan@gmail.com>
2015-03-16 16:53:03 +0800

    Add the top k problem solution.

--- core/src/main/java/yanglifan/learn/core/TopKHandler.java
+++ core/src/main/java/yanglifan/learn/core/TopKHandler.java
@@ -1,116 +1,22 @@
 package yanglifan.learn.core;
 
-import org.junit.Test;
-
 import java.util.*;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 public class TopKHandler {
-    private final static int TOTAL = 10000000;
-
     Set<Integer> indexRecord = new HashSet<>();
+    Set<Integer> heapRecord = new HashSet<>();
     int[] source;
-
-    public TopKHandler() {
-    }
 
     public TopKHandler(int[] source) {
         this.source = source;
     }
 
-    private int[] randomArray(int size) {
-        Random random = new Random();
-        int[] array = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            int a = random.nextInt();
-            array[i] = a;
-        }
-
-        return array;
-    }
-
-    private int[] orderArray(int size) {
-        int[] array = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            array[i] = i;
-        }
-
-        return array;
-    }
-
-//    @Test
-    public void test_random() {
-        System.out.println("========== main_test ==========");
-        int[] source = randomArray(10000000);
-        TopKHandler topKHandlerHandler = new TopKHandler(source);
-        Integer[] topK = topKHandlerHandler.getTopK(100);
-        for (int i : topK) {
-            System.out.println(i);
-        }
-    }
-
-//    @Test
-    public void test_order_array() {
-        System.out.println("========== test_order_array ==========");
-        int[] source = orderArray(10_000_000);
-        TopKHandler topKHandlerHandler = new TopKHandler(source);
-        Integer[] topK = topKHandlerHandler.getTopK(100);
-        StringBuilder builder = new StringBuilder();
-        for (int i : topK) {
-            builder.append(i);
-            if (i != 9_999_999) {
-                builder.append(", ");
-            }
-        }
-        System.out.println("Test Order Array result is " + builder.toString());
-    }
-
-    @Test
-    public void simple_test_1() {
-        System.out.println("========== simple_test_1 ==========");
-        int[] source = new int[]{1, 2, 3, 4, 5, 6};
-        TopKHandler topKHandlerHandler = new TopKHandler(source);
-        Integer[] topK = topKHandlerHandler.getTopK(2);
-        assertThat(topK, is(new Integer[]{6, 5}));
-    }
-
-    @Test
-    public void simple_test_2() {
-        System.out.println("========== simple_test_2 ==========");
-        int[] source = new int[]{0, 0, 0, 0, 0};
-        TopKHandler topKHandlerHandler = new TopKHandler(source);
-        Integer[] topK = topKHandlerHandler.getTopK(2);
-        assertThat(topK, is(new Integer[]{0, 0}));
-    }
-
-    @Test
-    public void simple_test_3() {
-        System.out.println("========== simple_test_3 ==========");
-        int[] source = new int[]{1, 5, 18, -4, 99, 3, 0, 67, 1, 2, 11};
-        TopKHandler topKHandlerHandler = new TopKHandler(source);
-        Integer[] topK = topKHandlerHandler.getTopK(3);
-        assertThat(topK, is(new Integer[]{99, 67, 18}));
-    }
-
     /**
-     * This is the main method to implement the top k function.
-     *
-     * @param k
-     * @return
+     * This is the main method to implement the top k function. Use heap sort to find the top k.
      */
-    public Integer[] getTopK(int k) {
-        Comparator<Integer> comparator = new Comparator<Integer>() {
-            @Override
-            public int compare(Integer integer, Integer t1) {
-                return t1 - integer;
-            }
-        };
-
-        PriorityQueue<Integer> heap = new PriorityQueue<>(comparator);
+    public Integer[] solve(int k) {
+        Comparator<Integer> descComparator = (t1, t2) -> t2 - t1;
+        PriorityQueue<Integer> heap = new PriorityQueue<>(descComparator);
 
         generateRandomHeap(source, k, heap);
         Integer[] topK = getInitTopK(heap, k);
@@ -118,29 +24,25 @@ public class TopKHandler {
 
         for (int i = 0; i < source.length; i++) {
             if (reachGroupSize(k, i) || reachTotalSize(source, i)) {
-                addIntoHeap(heap, source[i], bound);
+                addIntoHeap(heap, i, bound);
                 addPreTopKIntoHeap(heap, topK);
-
-                for (int j = 0; j < k; j++) {
-                    Integer heapElement = heap.poll();
-
-                    if (heapElement == null) continue;
-
-                    topK[j] = heapElement;
-
-                    System.out.println(topK[j]);
-                    if (j == k - 1) {
-                        System.out.println("Update the down bound as " + topK[j]);
-                        bound = topK[j];
-                    }
-                }
-                heap = new PriorityQueue<>(comparator);
+                getTopK(k, heap, topK);
+                bound = topK[k - 1];
+                heap = new PriorityQueue<>(descComparator);
             } else {
-                addIntoHeap(heap, source[i], bound);
+                addIntoHeap(heap, i, bound);
             }
         }
 
         return topK;
+    }
+
+    private void getTopK(int k, PriorityQueue<Integer> heap, Integer[] topK) {
+        for (int j = 0; j < k; j++) {
+            Integer heapElement = heap.poll();
+            if (heapElement == null) continue;
+            topK[j] = heapElement;
+        }
     }
 
     private Integer[] getInitTopK(PriorityQueue<Integer> heap, int k) {
@@ -178,10 +80,14 @@ public class TopKHandler {
         return i % groupSize == groupSize - 1;
     }
 
-    private void addIntoHeap(PriorityQueue<Integer> heap, int value, Integer bound) {
-        if (bound == null || value > bound) {
-            System.out.println("Add " + value + " into the heap");
-            heap.add(value);
+    private void addIntoHeap(PriorityQueue<Integer> heap, int index, Integer bound) {
+        if (indexRecord.contains(index)) return;
+
+        if (heapRecord.contains(source[index])) return;
+
+        if (bound == null || source[index] > bound) {
+            heap.add(source[index]);
+            heapRecord.add(source[index]);
         }
     }
 }
--- core/src/test/java/yanglifan/learn/core/TopKHandlerTest.java
+++ core/src/test/java/yanglifan/learn/core/TopKHandlerTest.java
@@ -1,7 +1,102 @@
 package yanglifan.learn.core;
 
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class TopKHandlerTest {
+    @Test
+    public void simple_test_1() {
+        System.out.println("========== simple_test_1 ==========");
+        int[] source = new int[]{1, 2, 3, 4, 5, 6};
+        TopKHandler topKHandlerHandler = new TopKHandler(source);
+        Integer[] topK = topKHandlerHandler.solve(2);
+        assertThat(topK, is(new Integer[]{6, 5}));
+    }
 
+    @Test
+    public void simple_test_2() {
+        System.out.println("========== simple_test_2 ==========");
+        int[] source = new int[]{0, 0, 0, 0, 0};
+        TopKHandler topKHandlerHandler = new TopKHandler(source);
+        Integer[] topK = topKHandlerHandler.solve(2);
+        assertThat(topK, is(new Integer[]{0, 0}));
+    }
+
+    @Test
+    public void simple_test_3() {
+        System.out.println("========== simple_test_3 ==========");
+        int[] source = new int[]{1, 5, 18, -4, 99, 3, 0, 67, 1, 2, 11};
+        TopKHandler topKHandlerHandler = new TopKHandler(source);
+        Integer[] topK = topKHandlerHandler.solve(3);
+        assertThat(topK, is(new Integer[]{99, 67, 18}));
+    }
+
+    @Test
+    public void test_random_array() {
+        System.out.println("========== main_test ==========");
+        int[] source = randomArray(10_000_000);
+        long start = System.currentTimeMillis();
+        TopKHandler topKHandlerHandler = new TopKHandler(source);
+        Integer[] topK = topKHandlerHandler.solve(100);
+        System.out.println("Time spend is " + (System.currentTimeMillis() - start) / 1000 + " s");
+        printResult(topK);
+    }
+
+    /**
+     * Asc array is the worst condition.
+     *
+     * If do not consider duplicate element, the time spend is less than 3s.
+     * 4 cores vm.
+     */
+    @Test
+    public void test_order_array() {
+        System.out.println("========== test_order_array ==========");
+        int[] source = orderArray(10_000_000);
+        long start = System.currentTimeMillis();
+        TopKHandler topKHandlerHandler = new TopKHandler(source);
+        Integer[] topK = topKHandlerHandler.solve(100);
+        System.out.println("Time spend is " + (System.currentTimeMillis() - start) / 1000 + " s");
+        printResult(topK);
+
+        assert topK[0] == 9_999_999;
+        assert topK[99] == 9_999_900;
+    }
+
+    private void printResult(Integer[] topK) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < topK.length; i++) {
+            builder.append(topK[i]);
+            if (i != 99) {
+                builder.append(", ");
+            }
+        }
+
+        System.out.println("Test Order Array result is " + builder.toString());
+    }
+
+    private int[] randomArray(int size) {
+        Random random = new Random();
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            int a = random.nextInt(10_000_000);
+            array[i] = a;
+        }
+
+        return array;
+    }
+
+    private int[] orderArray(int size) {
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = i;
+        }
+
+        return array;
+    }
 }
