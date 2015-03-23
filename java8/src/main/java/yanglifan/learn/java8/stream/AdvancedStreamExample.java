3b9c26af7891c50ec1c484d5c138fc6268b0f9eb
Lifan Yang <yanglifan@gmail.com>
2015-03-23 17:40:40 +0800

    Advanced stream example

---
+++ java8/src/main/java/yanglifan/learn/java8/stream/AdvancedStreamExample.java
@@ -0,0 +1,35 @@
+package yanglifan.learn.java8.stream;
+
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AdvancedStreamExample {
+    @Test
+    public void flat_map() throws Exception {
+        String filePath = getClass().getClassLoader().getResource("stuff.txt").getPath();
+        List<String> distinctElements = Files.lines(Paths.get(filePath))
+                .map(line -> line.split("\\s")) // Stream<String[]>
+                .flatMap(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
+        assertThat(distinctElements, is(Arrays.asList("aaa", "bbb", "ccc", "ddd")));
+
+        List<String> distinctElements2 = Files.lines(Paths.get(filePath))
+                .flatMap(line -> {
+                    String[] words = line.split("\\s");
+                    return Arrays.stream(words);
+                })
+                .distinct()
+                .collect(Collectors.toList());
+        assertThat(distinctElements2, is(Arrays.asList("aaa", "bbb", "ccc", "ddd")));
+    }
+}
