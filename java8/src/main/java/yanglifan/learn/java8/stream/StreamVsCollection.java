790de4f779890e5456fe8dacf8d606ab913db6fc
Yang Lifan <yanglifan@gmail.com>
2015-03-25 18:58:31 +0800

    Implement the 2nd exercise in http://docs.oracle.com/javase/tutorial/collections/streams/QandE/questions.html

--- java8/src/main/java/yanglifan/learn/java8/stream/StreamVsCollection.java
+++ java8/src/main/java/yanglifan/learn/java8/stream/StreamVsCollection.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.util.*;
 
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StreamVsCollection {
     private List<Transaction> transactions;
 
@@ -53,5 +57,83 @@ public class StreamVsCollection {
                 .sorted(Comparator.comparing(Transaction::getValue).reversed())
                 .map(Transaction::getId)
                 .forEach(System.out::println);
+    }
+
+    /**
+     * Implement the 2nd exercise in http://docs.oracle.com/javase/tutorial/collections/streams/QandE/questions.html
+     */
+    @Test
+    public void jdk_tutorial_exercise() {
+        Collection<Album> source = albums();
+
+        /**
+         * Old way process.
+         */
+        List<Album> oldAlbums = new ArrayList<>();
+        for (Album a : source) {
+            boolean hasFavorite = false;
+            for (Track t : a.tracks) {
+                if (t.rating >= 4) {
+                    hasFavorite = true;
+                    break;
+                }
+            }
+            if (hasFavorite)
+                oldAlbums.add(a);
+        }
+        Collections.sort(oldAlbums, new Comparator<Album>() {
+            public int compare(Album a1, Album a2) {
+                return a1.name.compareTo(a2.name);
+            }});
+
+        /**
+         * Stream way process.
+         */
+        List<Album> streamAlbums = source.stream()
+                .filter(a -> a.tracks.stream().anyMatch(t -> t.rating >= 4))
+                .sorted((a1, a2) -> a1.name.compareTo(a2.name))
+                .collect(toList());
+
+        assertThat(streamAlbums, is(oldAlbums));
+    }
+
+    Collection<Album> albums() {
+        Collection<Album> albums = new ArrayList<>();
+        Album album = new Album("Coast To Coast");
+        album.tracks.add(new Track(5));
+        album.tracks.add(new Track(4));
+        album.tracks.add(new Track(3));
+        albums.add(album);
+
+        album = new Album("Black & Blue");
+        album.tracks.add(new Track(5));
+        album.tracks.add(new Track(4));
+        album.tracks.add(new Track(3));
+        albums.add(album);
+
+        album = new Album("Borrowed Heaven");
+        album.tracks.add(new Track(3));
+        album.tracks.add(new Track(3));
+        albums.add(album);
+
+        return albums;
+    }
+}
+
+class Album {
+    String name;
+
+    Collection<Track> tracks = new ArrayList<>();
+
+    public Album(String name) {
+        this.name = name;
+    }
+}
+
+class Track {
+    int rating;
+
+    public Track(int rating) {
+        this.rating = rating;
     }
 }
