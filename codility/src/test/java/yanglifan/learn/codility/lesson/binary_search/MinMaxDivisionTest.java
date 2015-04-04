bc4884e6d52e2e71642c967eaf5e3ed8615ef014
Lifan Yang <yanglifan@gmail.com>
2015-04-04 16:34:53 +0800

    Lesson 12 - MinMaxDivision

---
+++ codility/src/test/java/yanglifan/learn/codility/lesson/binary_search/MinMaxDivisionTest.java
@@ -0,0 +1,41 @@
+package yanglifan.learn.codility.lesson.binary_search;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Parameterized.class)
+public class MinMaxDivisionTest {
+
+    int k;
+    int m;
+    int[] a;
+    int r;
+
+    public MinMaxDivisionTest(int k, int m, int[] a, int r) {
+        this.k = k;
+        this.m = m;
+        this.a = a;
+        this.r = r;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {3, 5, new int[]{2, 1, 5, 1, 2, 2, 2}, 6},
+                {3, 5, new int[]{1, 1, 1, 1, 1, 1}, 2}
+        });
+    }
+
+    @Test
+    public void testSolution() throws Exception {
+        System.out.println("========");
+        assertThat(MinMaxDivision.solution(k, m, a), is(r));
+    }
+}
