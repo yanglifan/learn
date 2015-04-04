2a12c0bc639ebb5054f39e9e2c460f24a57e84f2
Lifan Yang <yanglifan@gmail.com>
2015-04-04 16:44:34 +0800

    Very easy string test

---
+++ codility/src/main/java/yanglifan/learn/codility/lesson/string/ReverseString.java
@@ -0,0 +1,28 @@
+package yanglifan.learn.codility.lesson.string;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ReverseString {
+    public static String solution(String source) {
+        char[] chars = source.toCharArray();
+        char tmp;
+        for (int i = 0; i < chars.length / 2; i++) {
+            tmp = chars[i];
+            chars[i] = chars[chars.length - i - 1];
+            chars[chars.length - i - 1] = tmp;
+        }
+
+        return String.valueOf(chars);
+    }
+
+    @Test
+    public void test() {
+        assertThat(solution("abcd"), is("dcba"));
+        assertThat(solution("abcdf"), is("fdcba"));
+        assertThat(solution("a"), is("a"));
+        assertThat(solution(""), is(""));
+    }
+}
