0a153e11b5eb3b06c62bbf9214f895a8fcbafd45
Yang Lifan <yanglifan@gmail.com>
2015-06-10 17:51:54 +0800

    Add Thrift Swift async invocation demo

--- framework/src/main/java/yanglifan/learn/framework/thrift/swift/Service.java
+++ framework/src/main/java/yanglifan/learn/framework/thrift/swift/Service.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import com.facebook.swift.service.ThriftMethod;
 import com.facebook.swift.service.ThriftService;
+import com.google.common.util.concurrent.ListenableFuture;
 
 @ThriftService
-public interface Scribe {
+public interface Service {
     @ThriftMethod
     ResultCode log(List<LogEntry> messages);
+
+    @ThriftMethod
+    ListenableFuture<String> asyncMethod(String message);
 }
--- framework/src/main/java/yanglifan/learn/framework/thrift/swift/SwiftService.java
+++ framework/src/main/java/yanglifan/learn/framework/thrift/swift/SwiftService.java
@@ -3,6 +3,7 @@ package yanglifan.learn.framework.thrift.swift;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 import com.facebook.nifty.client.FramedClientConnector;
 import com.facebook.nifty.processor.NiftyProcessor;
@@ -11,15 +12,21 @@ import com.facebook.swift.service.ThriftClientManager;
 import com.facebook.swift.service.ThriftServer;
 import com.facebook.swift.service.ThriftServerConfig;
 import com.facebook.swift.service.ThriftServiceProcessor;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static com.google.common.net.HostAndPort.fromParts;
 
-public class SwiftScribe implements Scribe {
-    private static final Logger LOGGER = LoggerFactory.getLogger(SwiftScribe.class);
+public class SwiftService implements Service {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SwiftService.class);
 
     private final List<LogEntry> messages = new ArrayList<>();
+
+    private ListeningExecutorService listeningExecutorService = MoreExecutors.listeningDecorator(
+            Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()));
 
     public List<LogEntry> getMessages() {
         return messages;
@@ -32,8 +39,18 @@ public class SwiftScribe implements Scribe {
         return ResultCode.OK;
     }
 
+    @Override
+    public ListenableFuture<String> asyncMethod(String message) {
+        LOGGER.info("Async method has received a message [{}]", message);
+        return listeningExecutorService.submit(() -> {
+            Thread.sleep(5000);
+            LOGGER.info("Finish the process for [{}]", message);
+            return "The result of [" + message + "]";
+        });
+    }
+
     public static void main(String[] args) throws Exception {
-        SwiftScribe scribeService = new SwiftScribe();
+        SwiftService scribeService = new SwiftService();
         NiftyProcessor processor = new ThriftServiceProcessor(new ThriftCodecManager(), Collections.emptyList(), scribeService);
         ThriftServer server = new ThriftServer(processor, new ThriftServerConfig().setPort(8899));
         server.start();
@@ -41,8 +58,13 @@ public class SwiftScribe implements Scribe {
 
         ThriftClientManager clientManager = new ThriftClientManager();
         FramedClientConnector connector = new FramedClientConnector(fromParts("localhost", 8899));
-        Scribe scribe = clientManager.createClient(connector, Scribe.class).get();
-        scribe.log(Collections.singletonList(new LogEntry("c1", "m1")));
+        Service swiftClient = clientManager.createClient(connector, Service.class).get();
+        swiftClient.log(Collections.singletonList(new LogEntry("c1", "m1")));
+
+        ListenableFuture<String> listenableFuture = swiftClient.asyncMethod("Message A");
+        LOGGER.info("Receive the future");
+        String result = listenableFuture.get();
+        LOGGER.info("Receive the result [{}]", result);
 
         server.close();
     }
