44e9c2a36899b2f34c6e6b7a290e2c0b121fe0d7
杨理帆 <yanglifan@gmail.com>
2017-01-18 22:00:50 +0800

    Demo Java 8 method reference

---
+++ java8/src/main/java/yanglifan/learn/java8/MethodReferenceDemo.java
@@ -0,0 +1,28 @@
+package yanglifan.learn.java8;
+
+import org.junit.Test;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MethodReferenceDemo {
+
+    @Test
+    public void demo() {
+        Integer example = 1;
+
+        Supplier<String> stringSupplier = example::toString;
+        assertThat(stringSupplier.get(), is(example.toString()));
+
+
+        // We can bind to static method, here the generic parameter are the method parameter and return type
+        Supplier<Long> s1 = System::currentTimeMillis;
+
+        Function<Integer, Integer> f = example::compareTo;
+
+        assertThat(f.apply(2), is(-1));
+    }
+}
