a9ac71a0d7ee396574f2dd42579f696e9ba30ea3
杨理帆 <yanglifan@gmail.com>
2017-04-18 18:00:28 +0800

    How native threads handle ex

--- java-concurrency/src/main/java/yanglifan/learn/concurrency/ExceptionHandlerDemo.java
+++ java-concurrency/src/main/java/yanglifan/learn/concurrency/ExceptionHandlerDemo.java
@@ -1,19 +1,20 @@
 package yanglifan.learn.concurrency;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ExceptionHandlerDemo {
     private final static Logger LOGGER = LoggerFactory.getLogger("Default Exception Handler");
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            LOGGER.error("======== NOTE ========", e);
-        });
+//        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+//            LOGGER.error("======== NOTE ========", e);
+//        });
         Executors.newSingleThreadExecutor().execute(() -> {
             throw new RuntimeException("My runtime exception");
         });
@@ -23,5 +24,24 @@ public class ExceptionHandlerDemo {
             throw new RuntimeException("My runtime exception");
         });
 //        f.get();
+    }
+
+    @Test
+    public void ex_handle_in_thread() throws Exception {
+        Thread t = new Thread(() -> System.out.println(1 / 0));
+        t.start();
+        Thread.sleep(10000000);
+    }
+
+    @Test
+    public void output_ex_to_log() {
+        Thread t = new Thread(() -> {
+            try {
+                System.out.println(1 / 0);
+            } catch (Exception e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+        });
+        t.start();
     }
 }
