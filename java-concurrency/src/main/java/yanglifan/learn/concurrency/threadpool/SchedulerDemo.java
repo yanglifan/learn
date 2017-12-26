9a010aa211a0d41e1417b314bc3b33739f3db601
Yang Lifan <yanglifan@gmail.com>
2017-12-26 13:00:52 +0800

    Demo throw exception in ScheduledThreadPool

---
+++ java-concurrency/src/main/java/yanglifan/learn/concurrency/threadpool/SchedulerDemo.java
@@ -0,0 +1,24 @@
+package yanglifan.learn.concurrency.threadpool;
+
+import org.junit.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Yang Lifan
+ */
+public class SchedulerDemo {
+    @Test
+    public void demo_exception() throws Exception {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(4);
+        AtomicInteger count = new AtomicInteger();
+        scheduler.scheduleAtFixedRate(() -> {
+            System.out.println(count.getAndIncrement());
+            throw new RuntimeException("except ex");
+        }, 0, 1, TimeUnit.SECONDS);
+        Thread.sleep(10000);
+    }
+}
